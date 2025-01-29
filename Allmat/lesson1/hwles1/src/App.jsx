eef8a8e5ea17d9e94584ed09b46208856f3aa015
teiimann <147412982+teiimann@users.noreply.github.com>
2025-01-29 20:01:39 +0500

    lesson1

---
+++ Allmat/lesson1/hwles1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from './Header';
+import CourseInfo from './CourseInfo';
+
+function App(){
+  return(
+    <div>
+      <Header/>
+      <CourseInfo/>
+    </div>
+  )
+}
+
+export default App;
---
+++ Allmat/lesson1/hwles1/src/CourseInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CourseInfo.css';
+
+function CourseInfo() {
+  return (
+    <div className="course-info">
+      <h2>Course Detail</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos sed beatae deleniti aliquam aliquid temporibus excepturi sequi ex eum aut, et optio totam vitae aspernatur nemo adipisci, natus dolorum pariatur.</p>
+      <button className="info-button">Learn More</button>
+    </div>
+  );
+}
+
+export default CourseInfo;
